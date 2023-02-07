85e592cbd9780d7d7533ae4b018f5c95238ee8be
huangxuwei <2117008741@qq.com>
2023-02-07 20:08:40 +0800

    1

--- dev-utils/src/main/java/com/wei/util/StreamUtils.java
+++ dev-utils/src/main/java/com/wei/util/StreamUtils.java
@@ -1,7 +1,95 @@
 package com.wei.util;
 
-public class StreamUtils {
-    private StreamUtils() {
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+/**
+ * 流 工具类
+ *
+ * @author PC
+ */
+public class StreamUtils {
+
+    private StreamUtils() {
+    }
+
+    /**
+     * 对象根据字段去重-用用于流中 filter
+     *
+     * @param keyExtractor 函数接口
+     * @param <T>          泛型
+     * @return 返回判断结果
+     */
+    public static <T> Predicate<T> distinctByAttr(Function<? super T, ?> keyExtractor) {
+        Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+        return t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null;
+    }
+
+    /**
+     * 对象根据字段去重-直接操作List
+     *
+     * @param keyExtractor 函数接口
+     * @param <T>          泛型
+     * @return 返回去重后的List
+     */
+    public static <T> List<T> distinctByField(List<T> list, Function<? super T, ?> keyExtractor) {
+        notNull(list);
+        Map<Object, Boolean> seen = new ConcurrentHashMap<>();
+        return list.stream().filter(t -> seen.putIfAbsent(keyExtractor.apply(t), Boolean.TRUE) == null).collect(Collectors.toList());
+    }
+
+    /**
+     * List<String> 转为 List<Long>
+     */
+    public static List<Long> stringToLong(List<String> list) {
+        notNull(list);
+        return list.stream().map(Long::parseLong).collect(Collectors.toList());
+    }
+
+    /**
+     * List<Long> 转为 List<String>
+     */
+    public static List<String> longToString(List<Long> list) {
+        notNull(list);
+        return list.stream().map(String::valueOf).collect(Collectors.toList());
+    }
+
+    /**
+     * jdk9  takeWhile模拟接口
+     */
+    public static <T> List<T> takeWhile(List<T> list, Predicate<T> p) {
+        int i = 0;
+        for (T item : list) {
+            if (!p.test(item)) {
+                return list.subList(0, i);
+            }
+            i++;
+        }
+        return list;
+    }
+
+    /**
+     * jdk9 dropWhile 模拟接口
+     */
+    public static <T> List<T> dropWhile(List<T> list, Predicate<T> p) {
+        notNull(list);
+        int i = 0;
+        for (T item : list) {
+            if (p.test(item)) {
+                return list.subList(i, list.size());
+            }
+            i++;
+        }
+        return list;
+    }
+
+    private static void notNull(Object object) {
+        if (object == null) {
+            throw new NullPointerException("list 不可为空");
+        }
     }
 }
