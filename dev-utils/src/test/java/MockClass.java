1c6dc0573f70a49009ab9bc2c18ade6e8920b385
huangxuwei <2117008741@qq.com>
2023-03-17 23:25:44 +0800

    1

---
+++ dev-utils/src/test/java/MockClass.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MockClass {
+    public static List<People> mockPeopleList() {
+        return Stream.of(new People("小黄", 1),
+                        new People("小张", 2),
+                        new People("小李", 3),
+                        new People("小宋", 4),
+                        new People("小旭", 5),
+                        new People("小伟", 6),
+                        new People("小大", 7),
+                        new People("小大", 8))
+                .collect(Collectors.toList());
+    }
+}
