53999ec82327daa35ef156ccb34c8acc441fd5cf
huangxuwei <2117008741@qq.com>
2023-03-29 23:25:13 +0800

    1

--- common-data/src/main/java/com/wei/mapper/TestMapper.java
+++ common-data/src/main/java/com/wei/mapper/TestMapper.java
@@ -1,11 +1,11 @@
 package com.wei.mapper;
 
 import com.wei.entity.TestUser;
-import org.springframework.stereotype.Repository;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Repository
+@Mapper
 public interface TestMapper {
     List<TestUser> queryUser();
 }
