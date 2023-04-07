c92e6b42dad1c7fe86da931e266d5e723cb2cdf0
huangxuwei <2117008741@qq.com>
2023-04-07 22:56:35 +0800

    1

--- jmh/src/main/java/com/wei/demo/JmhTimeTest.java
+++ jmh/src/main/java/com/wei/demo/JmhTimeTest.java
@@ -12,32 +12,32 @@ import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
 @Warmup(iterations = 3, time = 1)
-@Measurement(iterations = 5, time = 1)
-@Threads(5)
+@Measurement(iterations = 20, time = 1)
+@Threads(1)
 @Fork(1)
 @State(value = Scope.Benchmark)
 @OutputTimeUnit(TimeUnit.SECONDS)
 public class JmhTimeTest {
 
-    @Param(value = {"1", "2", "3", "4", "5"})
+    @Param(value = {"1", "2", "3", "4", "5","6"})
     private long length;
 
     @Benchmark
     public void sleepOnceSecond() {
 
-        ThreadUtils.safeSleep(length * 1000);
+        ThreadUtils.safeSleep(length * 100);
     }
 
     @Benchmark
     public void sleepTwiceSecond() {
-        ThreadUtils.safeSleep(length * 2000L);
+        ThreadUtils.safeSleep(length * 200L);
     }
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
                 .include(JmhTimeTest.class.getSimpleName())
-                .result("jmh/JmhTimeTest.text")
-                .resultFormat(ResultFormatType.TEXT).build();
+                .result("JmhTimeTest.json")
+                .resultFormat(ResultFormatType.JSON).build();
         new Runner(opt).run();
     }
 }
