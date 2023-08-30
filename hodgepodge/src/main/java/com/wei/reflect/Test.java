fe3d9a35951157c3572ff5d5228dedd2e8a574b2
huangxuwei <2117008741@qq.com>
2023-08-30 22:56:54 +0800

    1

--- hodgepodge/src/main/java/com/wei/reflect/Test.java
+++ hodgepodge/src/main/java/com/wei/reflect/Test.java
@@ -5,48 +5,47 @@ import com.esotericsoftware.reflectasm.MethodAccess;
 import java.lang.reflect.Method;
 
 public class Test {
+
+    private static final int param = 100000000;
+
     public static void main(String[] args) throws Exception {
 
         long now;
         long sum = 0;
         TestClass t = new TestClass();
         now = System.currentTimeMillis();
-        for (int i = 0; i < 500000; ++i) {
+        for (int i = 0; i < param; ++i) {
             t.setNum(i);
             sum += t.getNum();
         }
         System.out.println("get-set耗时" + (System.currentTimeMillis() - now) + "ms秒，和是" + sum);
         sum = 0;
         now = System.currentTimeMillis();
-        for (int i = 0; i < 500000; ++i) {
-            Class<?> c = Class.forName("com.wei.reflect.TestClass");
-            Class<?>[] argsType = new Class[1];
-            argsType[0] = int.class;
-            Method m = c.getMethod("setNum", argsType);
+        Class<?> c2 = Class.forName("com.wei.reflect.TestClass");
+        Class<?>[] argsType = new Class[1];
+        argsType[0] = int.class;
+        Method m = c2.getMethod("setNum", argsType);
+        for (int i = 0; i < param; ++i) {
             m.invoke(t, i);
             sum += t.getNum();
         }
         System.out.println("标准反射耗时" + (System.currentTimeMillis() - now) + "ms，和是" + sum);
         sum = 0;
         Class<?> c = Class.forName("com.wei.reflect.TestClass");
-        Class<?>[] argsType = new Class[1];
-        argsType[0] = int.class;
-        Method m = c.getMethod("setNum", argsType);
+        Class<?>[] argsTypeV2 = new Class[1];
+        argsTypeV2[0] = int.class;
         now = System.currentTimeMillis();
-        for (
-                int i = 0;
-                i < 500000; ++i) {
-            m.invoke(t, i);
+        Method m2 = c.getMethod("setNum", argsTypeV2);
+        for (int i = 0; i < param; ++i) {
+            m2.invoke(t, i);
             sum += t.getNum();
         }
         System.out.println("缓存反射耗时" + (System.currentTimeMillis() - now) + "ms，和是" + sum);
         sum = 0;
+        now = System.currentTimeMillis();
         MethodAccess ma = MethodAccess.get(TestClass.class);
         int index = ma.getIndex("setNum");
-        now = System.currentTimeMillis();
-        for (
-                int i = 0;
-                i < 500000; ++i) {
+        for (int i = 0; i < param; ++i) {
             ma.invoke(t, index, i);
             sum += t.getNum();
         }
