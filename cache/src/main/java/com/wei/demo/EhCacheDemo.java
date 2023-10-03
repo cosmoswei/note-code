578e4dd018c89870191bd137176a22eeefc80237
huangxuwei <2117008741@qq.com>
2023-10-03 13:04:00 +0800

    1

--- cache/src/main/java/com/wei/demo/EhCacheDemo.java
+++ cache/src/main/java/com/wei/demo/EhCacheDemo.java
@@ -16,7 +16,7 @@ public class EhCacheDemo {
                 .withCache("cache1",
                         CacheConfigurationBuilder
                                 .newCacheConfigurationBuilder(String.class,
-                                        Object.class,
+                                        String.class,
                                         ResourcePoolsBuilder.heap(1)
                                                 .offheap(1, MemoryUnit.GB)))  // 同时指定缓存的key-value类型，以及缓存容纳的个数
 
@@ -25,17 +25,14 @@ public class EhCacheDemo {
         cacheManager.init();
 
         //3. 获取Cache方式一。从缓存管理器拿到设置好的Cache
-        Cache<String, Object> cache1 = cacheManager.getCache("cache1", String.class, Object.class);
+        Cache<String, String> cache1 = cacheManager.getCache("cache1", String.class, String.class);
 
         //4. 获取Cache方式二。 基于缓存管理器构建一个Cache
-        Cache<String, Object> cache2 = cacheManager.createCache("cache2", CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, Object.class, ResourcePoolsBuilder.heap(5)));
-
+        Cache<String, String> cache2 = cacheManager.createCache("cache2", CacheConfigurationBuilder.newCacheConfigurationBuilder(String.class, String.class, ResourcePoolsBuilder.heap(5)));
 
         //5. 操作
         cache1.put("cache0", "阿巴阿巴~~");
         Object value1 = cache1.get("cache0");
         System.out.println(value1);
-
-
     }
 }
---
+++ cache/src/main/java/com/wei/demo/RedissionCacheDemo.java
@@ -0,0 +1,7 @@
+package com.wei.demo;
+
+public class RedissionCacheDemo {
+    public static void main(String[] args) {
+
+    }
+}
