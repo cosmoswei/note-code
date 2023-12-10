520c0531c38231c9935ac8de4d586dd3a362a7ff
huangxuwei <2117008741@qq.com>
2023-12-10 23:47:34 +0800

    1

--- algo/src/main/java/hello/TimeComplexity.java
+++ algo/src/main/java/hello/TimeComplexity.java
@@ -1,8 +1,10 @@
 package hello;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Stack;
 
-public class Test {
+public class TimeComplexity {
 
     /* for 循环 */
     int forLoop(int n) {
@@ -15,7 +17,7 @@ public class Test {
     }
 
     public static void main(String[] args) {
-        Test test = new Test();
+        TimeComplexity test = new TimeComplexity();
         int s = test.fib(100);
         System.out.println(s);
     }
@@ -100,5 +102,95 @@ public class Test {
             }
         }
         return count;
+    }
+
+    /* 指数阶（循环实现） */
+    int exponential(int n) {
+        int count = 0, base = 1;
+        // 细胞每轮一分为二，形成数列 1, 2, 4, 8, ..., 2^(n-1)
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < base; j++) {
+                count++;
+            }
+            base *= 2;
+        }
+        // count = 1 + 2 + 4 + 8 + .. + 2^(n-1) = 2^n - 1
+        return count;
+    }
+
+    /* 指数阶（递归实现） */
+    int expRecur(int n) {
+        if (n == 1)
+            return 1;
+        return expRecur(n - 1) + expRecur(n - 1) + 1;
+    }
+
+    /* 对数阶（循环实现） */
+    int logarithmic(float n) {
+        int count = 0;
+        while (n > 1) {
+            n = n / 2;
+            count++;
+        }
+        return count;
+    }
+
+    /* 对数阶（递归实现） */
+    int logRecur(float n) {
+        if (n <= 1)
+            return 0;
+        return logRecur(n / 2) + 1;
+    }
+
+    /* 线性对数阶 */
+    int linearLogRecur(float n) {
+        if (n <= 1)
+            return 1;
+        int count = linearLogRecur(n / 2) + linearLogRecur(n / 2);
+        for (int i = 0; i < n; i++) {
+            count++;
+        }
+        return count;
+    }
+
+
+    /* 阶乘阶（递归实现） */
+    int factorialRecur(int n) {
+        if (n == 0)
+            return 1;
+        int count = 0;
+        // 从 1 个分裂出 n 个
+        for (int i = 0; i < n; i++) {
+            count += factorialRecur(n - 1);
+        }
+        return count;
+    }
+
+    /* 生成一个数组，元素为 { 1, 2, ..., n }，顺序被打乱 */
+    int[] randomNumbers(int n) {
+        Integer[] nums = new Integer[n];
+        // 生成数组 nums = { 1, 2, 3, ..., n }
+        for (int i = 0; i < n; i++) {
+            nums[i] = i + 1;
+        }
+        // 随机打乱数组元素
+        Collections.shuffle(Arrays.asList(nums));
+        // Integer[] -> int[]
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = nums[i];
+        }
+        return res;
+    }
+
+    /* 查找数组 nums 中数字 1 所在索引 */
+    int findOne(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            // 当元素 1 在数组头部时，达到最佳时间复杂度 O(1)
+            // 当元素 1 在数组尾部时，达到最差时间复杂度 O(n)
+            if (nums[i] == 1)
+                return i;
+        }
+        return -1;
     }
 }
