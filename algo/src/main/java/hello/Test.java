77eaaa3769ba55f369f07bc41d94aa8130cc7c84
huangxuwei <2117008741@qq.com>
2023-12-10 23:40:12 +0800

    1

---
+++ algo/src/main/java/hello/Test.java
@@ -0,0 +1,104 @@
+package hello;
+
+import java.util.Stack;
+
+public class Test {
+
+    /* for 循环 */
+    int forLoop(int n) {
+        int res = 0;
+        // 循环求和 1, 2, ..., n-1, n
+        for (int i = 1; i <= n; i++) {
+            res += i;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        int s = test.fib(100);
+        System.out.println(s);
+    }
+
+    /* 双层 for 循环 */
+    String nestedForLoop(int n) {
+        StringBuilder res = new StringBuilder();
+        // 循环 i = 1, 2, ..., n-1, n
+        for (int i = 1; i <= n; i++) {
+            // 循环 j = 1, 2, ..., n-1, n
+            for (int j = 1; j <= n; j++) {
+                res.append("(" + i + ", " + j + "), ");
+            }
+        }
+        return res.toString();
+    }
+
+    /* 递归 */
+    int recur(int n) {
+        // 终止条件
+        if (n == 1)
+            return 1;
+        // 递：递归调用
+        int res = recur(n - 1);
+        // 归：返回结果
+        return n + res;
+    }
+
+    /* 尾递归 */
+    int tailRecur(int n, int res) {
+        // 终止条件
+        if (n == 0)
+            return res;
+        // 尾递归调用
+        return tailRecur(n - 1, res + n);
+    }
+
+    /* 斐波那契数列：递归 */
+    int fib(int n) {
+        // 终止条件 f(1) = 0, f(2) = 1
+        if (n == 1 || n == 2)
+            return n - 1;
+        // 递归调用 f(n) = f(n-1) + f(n-2)
+        int res = fib(n - 1) + fib(n - 2);
+        // 返回结果 f(n)
+        return res;
+    }
+
+    /* 使用迭代模拟递归 */
+    int forLoopRecur(int n) {
+        // 使用一个显式的栈来模拟系统调用栈
+        Stack<Integer> stack = new Stack<>();
+        int res = 0;
+        // 递：递归调用
+        for (int i = n; i > 0; i--) {
+            // 通过“入栈操作”模拟“递”
+            stack.push(i);
+        }
+        // 归：返回结果
+        while (!stack.isEmpty()) {
+            // 通过“出栈操作”模拟“归”
+            res += stack.pop();
+        }
+        // res = 1+2+3+...+n
+        return res;
+    }
+
+    /* 平方阶（冒泡排序） */
+    int bubbleSort(int[] nums) {
+        int count = 0; // 计数器
+        // 外循环：未排序区间为 [0, i]
+        for (int i = nums.length - 1; i > 0; i--) {
+            // 内循环：将未排序区间 [0, i] 中的最大元素交换至该区间的最右端
+            for (int j = 0; j < i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    // 交换 nums[j] 与 nums[j + 1]
+                    int tmp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = tmp;
+                    count += 3; // 元素交换包含 3 个单元操作
+                }
+            }
+        }
+        return count;
+    }
+}
