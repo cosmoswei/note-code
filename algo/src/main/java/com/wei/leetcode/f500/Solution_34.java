2fec584f22ef79b0795359b53fc24c6e1dc0c88f
huangxuwei <2117008741@qq.com>
2024-01-11 21:59:02 +0800

    1

--- algo/src/main/java/com/wei/leetcode/f500/Solution_34.java
+++ algo/src/main/java/com/wei/leetcode/f500/Solution_34.java
@@ -5,25 +5,15 @@ public class Solution_34 {
     public static void main(String[] args) {
         Solution_34 solution26 = new Solution_34();
         int[] nums = {2, 2};
-        int[] ints = solution26.searchRange(nums, 3);
+        int[] ints = solution26.searchRange(nums, 2);
         for (int s = 0; s < ints.length; s++) {
             System.out.println("s = " + ints[s]);
         }
     }
 
     public int[] searchRange(int[] nums, int target) {
-        int length = nums.length;
-        if (length == 0) {
-            return new int[]{-1, -1};
-        }
-        if (length == 1) {
-            if (nums[0] == target) {
-                return new int[]{0, 0};
+        int length = nums.length - 1;
 
-            } else {
-                return new int[]{-1, -1};
-            }
-        }
         int left = 0;
         int right = length;
         int leftInd;
@@ -39,7 +29,13 @@ public class Solution_34 {
                 left = mid + 1;
             }
         }
-        leftInd = (nums[left] == target) ? left : -1;
+        // 判断 target 是否存在于 nums 中
+        if (left < 0 || left >= nums.length) {
+            leftInd = -1;
+        } else {
+            leftInd = nums[left] == target ? left : -1;
+        }
+        // 判断一下 nums[left] 是不是 target
 
         left = 0;
         right = length;
@@ -54,13 +50,87 @@ public class Solution_34 {
                 left = mid + 1;
             }
         }
-        if (right == -1 || left == -1) {
-            return new int[]{-1, -1};
+        if (right < 0 || right >= nums.length) {
+            rightInd = -1;
+        } else {
+            rightInd = nums[right] == target ? right : -1;
         }
-        rightInd = (nums[right] == target) ? right : -1;
         return new int[]{leftInd, rightInd};
+//        return new int[]{left_bound(nums, target), right_bound(nums, target)};
     }
 
+
+    // 找到左边界
+//        while (left <= right) {
+//            int mid = (left + right) / 2;
+//            if (nums[mid] == target) {
+//                right = mid - 1;
+//            } else if (nums[mid] > target) {
+//                right = mid - 1;
+//            } else if (nums[mid] < target) {
+//                left = mid + 1;
+//            }
+//        }
+//        leftInd = (nums[left] == target) ? left : -1;
+
+    int left_bound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                // 别返回，锁定左侧边界
+                right = mid - 1;
+            }
+        }
+        // 判断 target 是否存在于 nums 中
+        if (left < 0 || left >= nums.length) {
+            return -1;
+        }
+        // 判断一下 nums[left] 是不是 target
+        return nums[left] == target ? left : -1;
+    }
+
+
+    // 找到右边界
+//        while (left <= right) {
+//        int mid = (left + right) / 2;
+//        if (nums[mid] == target) {
+//            left = mid + 1;
+//        } else if (nums[mid] > target) {
+//            right = mid - 1;
+//        } else if (nums[mid] < target) {
+//            left = mid + 1;
+//        }
+//    }
+    int right_bound(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                // 别返回，锁定右侧边界
+                left = mid + 1;
+            }
+        }
+        // 判断 target 是否存在于 nums 中
+        // if (left - 1 < 0 || left - 1 >= nums.length) {
+        //     return -1;
+        // }
+
+        // 由于 while 的结束条件是 right == left - 1，且现在在求右边界
+        // 所以用 right 替代 left - 1 更好记
+        if (right < 0 || right >= nums.length) {
+            return -1;
+        }
+        return nums[right] == target ? right : -1;
+    }
 
     public int[] searchRange1(int[] nums, int target) {
         int leftIdx = binarySearch(nums, target, true);
