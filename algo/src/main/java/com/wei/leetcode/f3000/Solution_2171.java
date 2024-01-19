29d3e74ad0140017269389d2d4f31c35b0dfbd8c
huangxuwei <2117008741@qq.com>
2024-01-19 20:28:15 +0800

    1

---
+++ algo/src/main/java/com/wei/leetcode/f3000/Solution_2171.java
@@ -0,0 +1,35 @@
+package com.wei.leetcode.f3000;
+
+
+import java.util.Arrays;
+
+public class Solution_2171 {
+
+    public static void main(String[] args) {
+        Solution_2171 solution = new Solution_2171();
+        int[] param = {4, 1, 6, 5};
+        long i = solution.minimumRemoval2(param);
+        System.out.println("i = " + i);
+    }
+
+    public long minimumRemoval(int[] beans) {
+        return 1L;
+    }
+
+    public long minimumRemoval2(int[] beans) {
+        int n = beans.length;
+        Arrays.sort(beans);
+        long total = 0; // 豆子总数
+        for (int i = 0; i < n; i++) {
+            total += beans[i];
+        }
+        long res = total; // 最少需要移除的豆子数
+        for (int i = 0; i < n; i++) {
+            res = Math.min(res, total - (long) beans[i] * (n - i));
+        }
+        return res;
+    }
+
+}
+
+
