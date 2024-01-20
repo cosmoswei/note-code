5401103f84927eb5e8e261813228ec466f4c839c
宇宙伟 <2117008741@qq.com>
2024-01-20 22:53:10 +0800

    1

---
+++ algo/src/main/java/com/wei/leetcode/f3000/Solution_2788.java
@@ -0,0 +1,46 @@
+package com.wei.leetcode.f3000;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution_2788 {
+
+    public static void main(String[] args) {
+        Solution_2788 solution = new Solution_2788();
+        List<String> req = Arrays.asList("$easy$","$problem$");
+        List<String> res = solution.splitWordsBySeparator(req, '$');
+        res.forEach(System.out::println);
+    }
+
+    public List<String> splitWordsBySeparator(List<String> words, char separator) {
+        List<String> res = new ArrayList<>();
+        int start = 0;
+        int end = 0;
+        for (String word : words) {
+            for (int i = 0; i < word.length(); i++) {
+                char charAt = word.charAt(i);
+                if (charAt == separator) {
+                    String substring = word.substring(start, end);
+                    if (substring.length() != 0) {
+                        res.add(substring);
+                    }
+                    start = i + 1;
+                }
+                if (i == word.length() - 1) {
+                    String substring = word.substring(start, end + 1);
+                    if (substring.length() != 0) {
+                        res.add(substring);
+                    }
+                }
+                end++;
+            }
+            start = 0;
+            end = 0;
+        }
+        return res;
+    }
+}
+
+
