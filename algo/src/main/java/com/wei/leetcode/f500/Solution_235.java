347ae12cae24c04020e33c7608dcb2223528fea4
宇宙伟 <2117008741@qq.com>
2024-02-25 22:58:11 +0800

    1

---
+++ algo/src/main/java/com/wei/leetcode/f500/Solution_235.java
@@ -0,0 +1,44 @@
+package com.wei.leetcode.f500;
+
+import com.wei.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution_235 {
+    /*
+    作者：力扣官方题解
+    链接：https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-search-tree/solutions/428633/er-cha-sou-suo-shu-de-zui-jin-gong-gong-zu-xian-26/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> path_p = getPath(root, p);
+        List<TreeNode> path_q = getPath(root, q);
+        TreeNode ancestor = null;
+        for (int i = 0; i < path_p.size() && i < path_q.size(); ++i) {
+            if (path_p.get(i) == path_q.get(i)) {
+                ancestor = path_p.get(i);
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+
+    public List<TreeNode> getPath(TreeNode root, TreeNode target) {
+        List<TreeNode> path = new ArrayList<TreeNode>();
+        TreeNode node = root;
+        while (node != target) {
+            path.add(node);
+            if (target.val < node.val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        path.add(node);
+        return path;
+    }
+}
+
