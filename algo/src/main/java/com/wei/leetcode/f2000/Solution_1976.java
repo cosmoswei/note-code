6e279bdb974cb74e1c78da5ebee077df3420edeb
宇宙伟 <2117008741@qq.com>
2024-03-05 22:16:10 +0800

    1

---
+++ algo/src/main/java/com/wei/leetcode/f2000/Solution_1976.java
@@ -0,0 +1,57 @@
+package com.wei.leetcode.f2000;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Solution_1976 {
+
+    /*
+    作者：力扣官方题解
+    链接：https://leetcode.cn/problems/number-of-ways-to-arrive-at-destination/solutions/951921/dao-da-mu-de-di-de-fang-an-shu-by-leetco-5ptp/
+    来源：力扣（LeetCode）
+    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public int countPaths(int n, int[][] roads) {
+        int mod = 1000000007;
+        List<int[]>[] e = new List[n];
+        for (int i = 0; i < n; i++) {
+            e[i] = new ArrayList<int[]>();
+        }
+        for (int[] road : roads) {
+            int x = road[0], y = road[1], t = road[2];
+            e[x].add(new int[]{y, t});
+            e[y].add(new int[]{x, t});
+        }
+        long[] dis = new long[n];
+        Arrays.fill(dis, Long.MAX_VALUE);
+        int[] ways = new int[n];
+
+        PriorityQueue<long[]> pq = new PriorityQueue<long[]>((a, b) -> Long.compare(a[0], b[0]));
+        pq.offer(new long[]{0, 0});
+        dis[0] = 0;
+        ways[0] = 1;
+
+        while (!pq.isEmpty()) {
+            long[] arr = pq.poll();
+            long t = arr[0];
+            int u = (int) arr[1];
+            if (t > dis[u]) {
+                continue;
+            }
+            for (int[] next : e[u]) {
+                int v = next[0], w = next[1];
+                if (t + w < dis[v]) {
+                    dis[v] = t + w;
+                    ways[v] = ways[u];
+                    pq.offer(new long[]{t + w, v});
+                } else if (t + w == dis[v]) {
+                    ways[v] = (ways[u] + ways[v]) % mod;
+                }
+            }
+        }
+        return ways[n - 1];
+    }
+}
+
