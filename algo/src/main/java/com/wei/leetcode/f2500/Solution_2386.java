ea86e6d456fee6e6165ff5f88bf4023ee4702607
宇宙伟 <2117008741@qq.com>
2024-03-09 21:35:05 +0800

    1

---
+++ algo/src/main/java/com/wei/leetcode/f2500/Solution_2386.java
@@ -0,0 +1,35 @@
+package com.wei.leetcode.f2500;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+class Solution_2386 {
+    public long kSum(int[] nums, int k) {
+        int n = nums.length;
+        long total = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= 0) {
+                total += nums[i];
+            } else {
+                nums[i] = -nums[i];
+            }
+        }
+        Arrays.sort(nums);
+
+        long ret = 0;
+        PriorityQueue<long[]> pq = new PriorityQueue<long[]>((a, b) -> Long.compare(a[0], b[0]));
+        pq.offer(new long[]{nums[0], 0});
+        for (int j = 2; j <= k; j++) {
+            long[] arr = pq.poll();
+            long t = arr[0];
+            int i = (int) arr[1];
+            ret = t;
+            if (i == n - 1) {
+                continue;
+            }
+            pq.offer(new long[]{t + nums[i + 1], i + 1});
+            pq.offer(new long[]{t - nums[i] + nums[i + 1], i + 1});
+        }
+        return total - ret;
+    }
+}
