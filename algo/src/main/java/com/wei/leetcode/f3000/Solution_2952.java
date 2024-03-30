15c201d3ade2e1b61b8b08f7abb9800b09820278
宇宙伟 <2117008741@qq.com>
2024-03-30 23:55:56 +0800

    1

---
+++ algo/src/main/java/com/wei/leetcode/f3000/Solution_2952.java
@@ -0,0 +1,22 @@
+package com.wei.leetcode.f3000;
+
+import java.util.Arrays;
+
+class Solution_2952 {
+    public int minimumAddedCoins(int[] coins, int target) {
+        Arrays.sort(coins);
+        int ans = 0;
+        int x = 1;
+        int length = coins.length, index = 0;
+        while (x <= target) {
+            if (index < length && coins[index] <= x) {
+                x += coins[index];
+                index++;
+            } else {
+                x *= 2;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
