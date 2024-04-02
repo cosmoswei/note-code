c2844e6995b1d9aebc679679a44448d314dd7164
宇宙伟 <2117008741@qq.com>
2024-04-02 23:28:00 +0800

    1

---
+++ algo/src/main/java/com/wei/leetcode/f1000/Solution_894.java
@@ -0,0 +1,30 @@
+package com.wei.leetcode.f1000;
+
+import com.wei.leetcode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution_894 {
+    public List<TreeNode> allPossibleFBT(int n) {
+        List<TreeNode> fullBinaryTrees = new ArrayList<TreeNode>();
+        if (n % 2 == 0) {
+            return fullBinaryTrees;
+        }
+        if (n == 1) {
+            fullBinaryTrees.add(new TreeNode(0));
+            return fullBinaryTrees;
+        }
+        for (int i = 1; i < n; i += 2) {
+            List<TreeNode> leftSubtrees = allPossibleFBT(i);
+            List<TreeNode> rightSubtrees = allPossibleFBT(n - 1 - i);
+            for (TreeNode leftSubtree : leftSubtrees) {
+                for (TreeNode rightSubtree : rightSubtrees) {
+                    TreeNode root = new TreeNode(0, leftSubtree, rightSubtree);
+                    fullBinaryTrees.add(root);
+                }
+            }
+        }
+        return fullBinaryTrees;
+    }
+}
