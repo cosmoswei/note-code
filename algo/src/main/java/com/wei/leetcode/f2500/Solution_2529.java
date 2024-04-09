10711ed2457c9777f9c72f6c4275c3ef59ebfc44
宇宙伟 <2117008741@qq.com>
2024-04-09 21:29:58 +0800

    1

---
+++ algo/src/main/java/com/wei/leetcode/f2500/Solution_2529.java
@@ -0,0 +1,22 @@
+package com.wei.leetcode.f2500;
+
+class Solution_2529 {
+    public int maximumCount(int[] nums) {
+        int pos1 = lowerBound(nums, 0);
+        int pos2 = lowerBound(nums, 1);
+        return Math.max(pos1, nums.length - pos2);
+    }
+
+    public int lowerBound(int[] nums, int val) {
+        int l = 0, r = nums.length;
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (nums[m] >= val) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}
