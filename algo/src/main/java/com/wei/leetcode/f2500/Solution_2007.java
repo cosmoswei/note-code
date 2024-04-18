a711bc4191c51fdbeebe43aed0da0a770e71a156
宇宙伟 <2117008741@qq.com>
2024-04-18 23:02:07 +0800

    1

---
+++ algo/src/main/java/com/wei/leetcode/f2500/Solution_2007.java
@@ -0,0 +1,29 @@
+package com.wei.leetcode.f2500;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution_2007 {
+    public int[] findOriginalArray(int[] changed) {
+        Arrays.sort(changed);
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int a : changed) {
+            count.put(a, count.getOrDefault(a, 0) + 1);
+        }
+        int[] res = new int[changed.length / 2];
+        int i = 0;
+        for (int a : changed) {
+            if (count.get(a) == 0) {
+                continue;
+            }
+            count.put(a, count.get(a) - 1);
+            if (count.getOrDefault(a * 2, 0) == 0) {
+                return new int[0];
+            }
+            count.put(a * 2, count.get(a * 2) - 1);
+            res[i++] = a;
+        }
+        return res;
+    }
+}
