c773a08f5f209c8a92798ecef4c8710c40837376
宇宙伟 <2117008741@qq.com>
2024-08-06 23:58:38 +0800

    1

--- metrics/src/main/java/com/wei/oshi/NetworkStats.java
+++ metrics/src/main/java/com/wei/oshi/NetworkStats.java
@@ -8,22 +8,17 @@ public class NetworkStats {
     public static void main(String[] args) throws InterruptedException {
         SystemInfo si = new SystemInfo();
         HardwareAbstractionLayer hal = si.getHardware();
-
         for (NetworkIF net : hal.getNetworkIFs()) {
             System.out.println("Interface: " + net.getDisplayName());
             net.updateAttributes();
             long rxBytesStart = net.getBytesRecv();
             long txBytesStart = net.getBytesSent();
-
             Thread.sleep(1000); // Sleep for 1 second
-
             net.updateAttributes();
             long rxBytesEnd = net.getBytesRecv();
             long txBytesEnd = net.getBytesSent();
-
-            long rxRate = (rxBytesEnd - rxBytesStart) / 1; // bytes per second
-            long txRate = (txBytesEnd - txBytesStart) / 1; // bytes per second
-
+            long rxRate = (rxBytesEnd - rxBytesStart); // bytes per second
+            long txRate = (txBytesEnd - txBytesStart); // bytes per second
             System.out.println("Receive Rate: " + rxRate + " B/s");
             System.out.println("Transmit Rate: " + txRate + " B/s");
         }
